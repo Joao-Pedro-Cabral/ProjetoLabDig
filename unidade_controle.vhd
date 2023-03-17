84c0d3ce7c024c8281384c087456217ad82610f0
Pedro Turini <pedro.turini@usp.br>
2023-03-17 13:28:07 -0300

    Novos estados adicionados

--- unidade_controle.vhd
+++ unidade_controle.vhd
@@ -18,6 +18,7 @@
 --     01/02/2023  1.3     Pedro H. Turini   adição do estado espera
 --     06/02/2022  1.4     João Pedro C.M.   versão desafio
 --     08/02/2023  1.5     João Pedro C.M.   exp5
+--     17/03/2023  1.6     Adaptacao 
 --------------------------------------------------------------------
 --
 library ieee;
@@ -34,11 +35,11 @@ entity unidade_controle is
         seleciona            : in  std_logic;
         enderecoIgualRodada  : in  std_logic;
         jogada_correta       : in  std_logic;
+        modo                 : in  std_logic_vector(1 downto 0);
         timeout              : in  std_logic;
         zeraCR               : out std_logic;
         contaCR              : out std_logic;
         limpaRC              : out std_logic;
-        contaI               : out std_logic;
         contaE               : out std_logic;
         zeraE                : out std_logic;
         zeraT                : out std_logic;
@@ -48,7 +49,9 @@ entity unidade_controle is
         pronto               : out std_logic;
 		escreve				 : out std_logic;
         registraSel          : out std_logic;
-        db_estado            : out std_logic_vector(3 downto 0)
+        escreve_aleatório    : out std_logic;
+        zeraI                : out std_logic;
+        db_estado            : out std_logic_vector(4 downto 0)
     );
 end entity;
 
@@ -89,7 +92,10 @@ begin
         fim_ganhou                when  Eatual=ultima_rodada and fimL = '1' else
 		espera_nova_jogada        when  Eatual=espera_nova_jogada and jogada = '0' else
 		escreve_jogada            when  Eatual=espera_nova_jogada and jogada = '1' else
+        mostra_jogada             when  Eatual=ultima_rodada and fimL='0' and modo!='10' else
+        mostra_jogada             when  Eatual=mostra_jogada and fimI='0' else
 		proxima_rodada			  when  Eatual=escreve_jogada else
+        proxima_rodada            when  Eatual=mostra_jogada and fimI='1' else   
         inicio_rodada             when  Eatual=proxima_rodada else
         fim_perdeu                when  Eatual=fim_perdeu and iniciar='0' else
         espera_selecao            when  Eatual=fim_perdeu and iniciar='1' else
@@ -97,6 +103,7 @@ begin
         espera_selecao            when  Eatual=fim_ganhou and iniciar='1' else
         fim_timeout               when  Eatual=fim_timeout and iniciar='0' else
         espera_selecao            when  Eatual=fim_timeout and iniciar='1' else
+        mostra_jogada             when 
         inicial; 
 
     -- logica de saída (maquina de Moore)
@@ -145,32 +152,30 @@ begin
 					'0' when others;
 
     with Eatual select
-        contaI    <=   '1' when inicializa_elementos,
-                       '0' when others;
-
-
-    with Eatual select
         registraSel    <=   '1' when registra_selecao,
                             '0' when others;
 
     -- saida de depuracao (db_estado)
     with Eatual select
-        db_estado <= "0000" when inicial,              -- 0
-                     "0001" when inicializa_elementos, -- 1
-                     "0010" when inicio_rodada,        -- 2
-                     "0011" when ultima_rodada,        -- 3
-                     "0100" when registra_jogada,      -- 4
-                     "0101" when compara_jogada,       -- 5
-                     "0110" when proxima_jogada,       -- 6
-                     "0111" when proxima_rodada,       -- 7
-					 "1000" when espera_nova_jogada,   -- 8
-					 "1001" when escreve_jogada,       -- 9
-                     "1010" when espera_selecao,       -- A
-                     "1011" when registra_selecao,     -- B
-                     "1100" when fim_ganhou,           -- C
-                     "1101" when fim_perdeu,           -- D
-                     "1110" when fim_timeout,          -- E
-                     "1111" when espera_jogada,        -- F
-                     "0000" when others;
+        db_estado <= "00000" when inicial,              -- 0
+                     "00001" when inicializa_elementos, -- 1
+                     "00010" when inicio_rodada,        -- 2
+                     "00011" when ultima_rodada,        -- 3
+                     "00100" when registra_jogada,      -- 4
+                     "00101" when compara_jogada,       -- 5
+                     "00110" when proxima_jogada,       -- 6
+                     "00111" when proxima_rodada,       -- 7
+					 "01000" when espera_nova_jogada,   -- 8
+					 "01001" when escreve_jogada,       -- 9
+                     "01010" when espera_dificuldade,   -- A
+                     "01011" when registra_dificuldade, -- B
+                     "01100" when fim_ganhou,           -- C
+                     "01101" when fim_perdeu,           -- D
+                     "01110" when fim_timeout,          -- E
+                     "01111" when espera_jogada,        -- F
+                     "10000" when espera_modo,          --10 
+                     "10001" when registra_modo,        --11
+                     "10010" when mostra_jogada,        --12
+                     "00000" when others;
 
 end architecture fsm;
